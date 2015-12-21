dc07e405c1af24c49c89cfa3fe7c13b49822aed0
LvBingru <lvbingru@gmail.com>
2015-12-21 21:54:09 +0800

    we chat share update

--- ios/RCTWeChat.h
+++ ios/RCTWeChat.h
@@ -9,6 +9,18 @@
 #import <Foundation/Foundation.h>
 #import "Base/RCTBridgeModule.h"
 
+#define RCTWXShareTypeNews @"news"
+#define RCTWXShareTypeImage @"image"
+#define RCTWXShareTypeText @"text"
+#define RCTWXShareTypeVideo @"video"
+#define RCTWXShareTypeAudio @"audio"
+
+#define RCTWXShareType @"type"
+#define RCTWXShareTitle @"title"
+#define RCTWXShareDescription @"description"
+#define RCTWXShareWebpageUrl @"webpageUrl"
+#define RCTWXShareImageUrl @"imageUrl"
+
 @interface RCTWeChat : NSObject <RCTBridgeModule>
 
 - (BOOL)handleUrl:(NSURL *)aUrl;
